de09525e4b0a102d9186a60dea34b5bbbdd75aec
罗超然 <manonloki@163.com>
2024-07-21 10:52:34 +0800

    :refactor auth middleware

--- chat_core/src/middlewares/auth.rs
+++ chat_core/src/middlewares/auth.rs
@@ -1,5 +1,5 @@
 use axum::{
-    extract::{FromRequestParts, Request, State},
+    extract::{FromRequestParts, Query, Request, State},
     http::StatusCode,
     middleware::Next,
     response::{IntoResponse, Response},
@@ -8,38 +8,54 @@ use axum_extra::{
     headers::{authorization::Bearer, Authorization},
     TypedHeader,
 };
+use serde::Deserialize;
 
 use super::TokenVerify;
+
+#[derive(Debug, Deserialize)]
+struct Params {
+    access_token: String,
+}
 
 pub async fn verify_token<T>(State(state): State<T>, req: Request, next: Next) -> Response
 where
     T: TokenVerify + Clone + Send + Sync + 'static,
 {
     let (mut parts, body) = req.into_parts();
-    let req =
-        match TypedHeader::<Authorization<Bearer>>::from_request_parts(&mut parts, &state).await {
-            Ok(TypedHeader(Authorization(bearer))) => {
-                let token = bearer.token();
 
-                match state.verify(token) {
-                    Ok(user) => {
-                        let mut req = Request::from_parts(parts, body);
-                        req.extensions_mut().insert(user);
-                        req
+    let token =
+        match TypedHeader::<Authorization<Bearer>>::from_request_parts(&mut parts, &state).await {
+            Ok(TypedHeader(Authorization(bearer))) => bearer.token().to_string(),
+            Err(e) => {
+                if e.is_missing() {
+                    match Query::<Params>::from_request_parts(&mut parts, &state).await {
+                        Ok(Query(params)) => params.access_token,
+                        Err(e) => {
+                            let msg = format!("Failed to parse query params: {:?}", e);
+                            tracing::warn!(msg);
+                            return (StatusCode::UNAUTHORIZED, msg).into_response();
+                        }
                     }
-                    Err(e) => {
-                        let msg = format!("Failed to verify token: {:?}", e);
-                        tracing::warn!(msg);
-                        return (StatusCode::FORBIDDEN, msg).into_response();
-                    }
+                } else {
+                    let msg = format!("Failed to parse Authorization header: {:?}", e);
+                    tracing::warn!(msg);
+                    return (StatusCode::UNAUTHORIZED, msg).into_response();
                 }
             }
-            Err(e) => {
-                let msg = format!("Failed to parse Authorization header: {:?}", e);
-                tracing::warn!(msg);
-                return (StatusCode::UNAUTHORIZED, msg).into_response();
-            }
         };
+
+    let req = match state.verify(&token) {
+        Ok(user) => {
+            let mut req = Request::from_parts(parts, body);
+            req.extensions_mut().insert(user);
+            req
+        }
+        Err(e) => {
+            let msg = format!("Failed to verify token: {:?}", e);
+            tracing::warn!(msg);
+            return (StatusCode::FORBIDDEN, msg).into_response();
+        }
+    };
 
     next.run(req).await
 }
@@ -93,17 +109,23 @@ mod tests {
             .route("/api", get(handler))
             .layer(from_fn_with_state(state.clone(), verify_token::<AppState>))
             .with_state(state);
-
+        // good token
         let req = Request::builder()
             .uri("/api")
             .header("Authorization", format!("Bearer {}", token))
+            .body(Body::empty())?;
+        let res = app.clone().oneshot(req).await?;
+        assert_eq!(res.status(), StatusCode::OK);
+
+        let req = Request::builder()
+            .uri(format!("/api?access_token={}", token))
             .body(Body::empty())?;
 
         let res = app.clone().oneshot(req).await?;
         assert_eq!(res.status(), StatusCode::OK);
 
+        // Bad Token
         let req = Request::builder().uri("/api").body(Body::empty())?;
-
         let res = app.clone().oneshot(req).await?;
         assert_eq!(res.status(), StatusCode::UNAUTHORIZED);
 
